4a889784fdb90c5ba1dd9a6d6b3360fbcf162cb9
danielrguezh <danielrh111005@gmail.com>
2024-06-06 19:33:27 +0100

    se crean las entidades

---
+++ demo/src/main/java/es/ies/puerto/modelo/entities/Cita.java
@@ -0,0 +1,9 @@
+package es.ies.puerto.modelo.entities;
+
+public class Cita {
+    int id_cita;
+    String fecha;
+    private int dni_paciente;
+    private int dni_medico;
+    private String valoracion;
+}
---
+++ demo/src/main/java/es/ies/puerto/modelo/entities/Paciente.java
@@ -0,0 +1,7 @@
+package es.ies.puerto.modelo.entities;
+
+public class Paciente {
+    private int dni_paciente;
+    public String nombre;
+    private String historial;
+}
